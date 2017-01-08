975744204ddd00fd867820fe5a6394208b29ae56
Salsadila <salsaoctavia2@gmail.com>
2017-01-08 11:58:22 +0700

    RecyclerView Delete

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
     public static String HOTEL;
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
+    private itempos
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -136,7 +138,19 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
 
     @Override
     public void doDelete(int pos) {
+        int itemPos = pos;
+        final Hotel hotel = mList.get(pos);
+        mList.remove(itemPos);
+        mAdapter.notifyDataSetChanged();
+        Snackbar undo = Snackbar.make(findViewById(R.id.fab), hotel.judul + " Terhapus", Snackbar.LENGTH_LONG)
+                .setAction("UNDO", new View.OnClickListener() {
 
+                    @Override
+                    public void onClick(View v) {
+                        mList.add(itempos, hotel);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }).show();
     }
 
     @Override
