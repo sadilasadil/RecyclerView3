2ba28036e3a704ba765bf72b1ae34a4b8fa22a8c
Salsadila <salsaoctavia2@gmail.com>
2017-01-10 22:59:37 +0700

    RecyclerView Search

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -7,9 +7,11 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,9 +27,12 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
     public static final int REQUEST_CODE_EDIT = 99;
     private static final int REQUEST_CODE_ADD = 88;
     public static String HOTEL;
+    ArrayList<Hotel> mListAll = new ArrayList<>();
+    boolean isFiltered;
+    ArrayList<Integer> mListMapFilter = new ArrayList<>();
+    String mQuery;
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
-    private itempos
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -103,7 +108,55 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView)
+                MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(
+                new SearchView.OnQueryTextListener() {
+
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        mQuery = newText.toLowerCase();
+                        doFilter(mQuery);
+                        return false;
+                    }
+                }
+        );
         return true;
+    }
+
+    private void doFilter(String mQuery) {
+        if (!isFiltered) {
+            mListAll.clear();
+            mListAll.addAll(mList);
+            isFiltered = true;
+        }
+
+        mList.clear();
+        if (mQuery == null || mQuery.isEmpty()) {
+            mList.addAll(mListAll);
+            isFiltered = false;
+        } else {
+            mListMapFilter.clear();
+            for (int i = 0; i < mListAll.size(); i++) {
+                Hotel hotel = mListAll.get(i);
+                if (hotel.judul.toLowerCase().contains(mQuery) ||
+                        hotel.deskripsi.toLowerCase().contains(mQuery) ||
+                        hotel.lokasi.toLowerCase().contains(mQuery)) {
+                    mList.add(hotel);
+                    mListMapFilter.add(i);
+                }
+
+            }
+        }
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -130,7 +183,6 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
 
     @Override
     public void doEdit(int pos) {
-        int itemPos = pos;
         Intent intent = new Intent(this, InputActivity.class);
         intent.putExtra(HOTEL, mList.get(pos));
         startActivityForResult(intent, REQUEST_CODE_EDIT);
@@ -138,16 +190,15 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
 
     @Override
     public void doDelete(int pos) {
-        int itemPos = pos;
         final Hotel hotel = mList.get(pos);
-        mList.remove(itemPos);
+        mList.remove(pos);
         mAdapter.notifyDataSetChanged();
         Snackbar undo = Snackbar.make(findViewById(R.id.fab), hotel.judul + " Terhapus", Snackbar.LENGTH_LONG)
                 .setAction("UNDO", new View.OnClickListener() {
 
                     @Override
                     public void onClick(View v) {
-                        mList.add(itempos, hotel);
+                        mList.add(pos, hotel);
                         mAdapter.notifyDataSetChanged();
                     }
                 }).show();
